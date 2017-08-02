b02f3b6e16efc464ede61fc10c3214083a8b1c71
wooxx074 <wooxx074@umn.edu>
2017-08-02 15:15:27 +0000

    Added summonerspell json to prevent api limiting in development. Commit before major changes to match_show_form partial

--- app/helpers/match_helper.rb
+++ app/helpers/match_helper.rb
@@ -7,16 +7,20 @@ module MatchHelper
     return result
   end
   
-  def retrieve_champion(id)
+  def current_game_version
     game_versions = json_parse("https://na1.api.riotgames.com/lol/static-data/v3/versions?api_key=#{ENV['riot_key']}")
     current_game_version = game_versions[0] #Find most current version of game for updated API
+    return "7.15.1" #current_game_version - For some reason Riot current has rate limits on static api calls should return current_game_version
+  end
+  def retrieve_champion(id)
+
     champlist = json_parse("http://ddragon.leagueoflegends.com/cdn/#{current_game_version}/data/en_US/champion.json")
     #champlist is a json file of every champ in the game
     #cycle through each champ in champlist["data"], k is the champion, v is the data within the champion
     #v is nested JSON data, look for "key" and match 
     champlist["data"].each do |k, v |
       if v["key"] == id.to_s
-        return v["id"]
+        return v
       end
     end
     #If no matches are available, return as "none"
@@ -25,13 +29,15 @@ module MatchHelper
   
   def retrieve_static_summs(id) #Other static data use ID number, but summoners need a different key
     source = "https://na1.api.riotgames.com/lol/static-data/v3/summoner-spells/#{id}?locale=en_US&api_key=#{ENV['riot_key']}"
-    summs_info = json_parse(source)
-    return summs_info["key"]
+    summs_info = JSON.parse(File.read "#{Rails.root}/app/helpers/summonerspell.json" )
+    summs_info["data"].each do |k, v |
+      if v["id"] == id
+        return k
+      end
+    end
   end
   
   def riot_static_img(category, value, imgsize) #Available categories: champion, spell, item, mastery, rune
-    game_versions = json_parse("https://na1.api.riotgames.com/lol/static-data/v3/versions?api_key=#{ENV['riot_key']}")
-    current_game_version = game_versions[0] #Find most current version of game for updated API
     return image_tag("http://ddragon.leagueoflegends.com/cdn/#{current_game_version}/img/#{category}/#{value}.png", size: imgsize)
   end
   
